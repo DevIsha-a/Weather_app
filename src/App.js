b84ed0b8b44fdfa24c1222cdd3e5dd042e3890bf
DevIsha-a <EishaTabassum0@gmail.com>
2024-06-05 16:25:27 +0500

    fixes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css'
 import { fetchData } from './FETCHDATA'
 
@@ -6,6 +6,13 @@ import { fetchData } from './FETCHDATA'
 export const App = () => {
   const [query, setquery] = useState('')
   const [weather, setWeather] = useState([])
+  useEffect(() => {
+    const getData = async () => {
+      const weather_data = await fetchData("Islamabad")
+      setWeather(weather_data) 
+    }
+    getData()
+  }, [])
 
   const search = async (e) => {
     if (e.key === 'Enter') {
@@ -20,7 +27,7 @@ export const App = () => {
     <div className='main-container'>
       <div className='search-container' id='container'>
         <input className='search' placeholder='search' value={query} onChange={e => setquery(e.target.value)} onKeyPress={search} />
-        {weather.main? (<div className='result_box' id='result'>
+        {weather.main ? (<div className='result_box' id='result'>
           <h2> <span>{weather.name}</span> <sup className='supr'>{weather.sys.country}</sup></h2>
           <div>
             <div><h1>{weather.main.temp}<sup>&deg;</sup>C</h1></div>
@@ -33,7 +40,7 @@ export const App = () => {
           <div><span>Feels like:</span> <span>{weather.main.feels_like}<sup>&deg;</sup>C</span></div>
           <div><span> Maximum temp:</span> <span>{weather.main.temp_max}<sup>&deg;</sup>C</span></div>
           <div><span> Minimum temp:</span> <span>{weather.main.temp_min}<sup>&deg;</sup>C</span></div>
-        </div>): <div className='result_box' id='result'>{weather} </div>}
+        </div>) : <div className='result_box' id='result'>{weather} </div>}
       </div>
     </div>
 
@@ -41,4 +48,3 @@ export const App = () => {
 
 }
 export default App
- 
